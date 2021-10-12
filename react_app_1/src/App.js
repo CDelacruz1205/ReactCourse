68a16ac76409bf4cf112aae93692e277d30b69b3
Carlos De la Cruz <delacruzc410@gmail.com>
2021-10-11 21:28:36 -0500

    ExpenseItem implementation

--- react_app_1/src/App.js
+++ react_app_1/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./Components/ExpenseItem/ExpenseItem";
 
 function App() {
+
+  // const expenses = [
+  //   {title: "Car Insurance", amount: 234.76, date: new Date(2021,5,19)},
+  //   {title: "Clothes Shopping", amount: 265.14, date: new Date(2021,1,12)},
+  //   {title: "Credit Card", amount: 8674.60, date: new Date(2021,12,25)},
+  // ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExpenseItem title="Hola" amount={3023.423} date={new Date(2021,5,19)}/>
     </div>
   );
 }
---
+++ react_app_1/src/Components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ExpenseDate from "./CalendarElement/ExpenseDate";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 2964.23;
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price"> ${props.amount} </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
